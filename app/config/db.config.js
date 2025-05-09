5deb5517f70be2f8c3fe3687665e8dcd2cfe5ed3
555088 <555088@mail.muni.cz>
2025-05-09 15:24:03 +0200

    Update3

--- app/config/db.config.js
+++ app/config/db.config.js
@@ -1,5 +1,6 @@
 module.exports = {
   HOST: "mysql-db-bb.mysql.database.azure.com",
+  port: "3306",
   USER: "boris",
   PASSWORD: "Cube123?",
   DB: "testdb",
--- app/models/db.js
+++ app/models/db.js
@@ -4,8 +4,9 @@ const dbConfig = require("../config/db.config.js");
 var connection = mysql.createPool({
   host: dbConfig.HOST,
   user: dbConfig.USER,
+  port: dbConfig.PORT,
   password: dbConfig.PASSWORD,
-  database: dbConfig.DB
+  database: dbConfig.DB,
 });
 
 module.exports = connection;
